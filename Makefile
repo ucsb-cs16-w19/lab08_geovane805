73117bca6720d939cf3fd926439bcfa4089e30b6
Geovane Marquez <geovane@cs.ucsb.edu>
2019-03-07 17:21:42 -0800

    isAnagram

---
+++ Makefile
@@ -0,0 +1,2 @@
+strFuncs: strFuncs.h strFuncs.cpp
+	g++ -o strFuncs strFuncs.cpp strFuncs.h
--- strFuncs.cpp
+++ strFuncs.cpp
@@ -11,7 +11,36 @@ using namespace std;
  * the case for the letters (upper or lower) should not
  * affect your result. 
  */
-bool isAnagram(string s1, string s2){
+bool isAnagram(string s1, string s2)
+{
+  int strlength = s1.length();
+  string temp = s1;
+  for (int i = 0; i < strlength; i++)
+  {
+    int countS1 = 0; //to count occurrances of s1
+    int countS2 = 0; //to count occurrances of s2
+    char c = s1[i]; //iterates through s1, char by char
+
+    for (int j = 0, k = 0; j < strlength; j++, k++)
+    {
+      if(c == temp[j])
+      {
+        countS1++;
+      }
+      if(c == s2[k])
+      {
+        countS2++;
+      }
+    }
+    if (countS1 == countS2)
+    {
+      continue;
+    }
+    else
+    {
+      return false;
+    }
+  }
   return true;
 }
 
