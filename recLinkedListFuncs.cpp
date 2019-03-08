4067712b5836d823303e7189e18dec9123b256e2
Geovane Marquez <geovane@cs.ucsb.edu>
2019-03-08 12:05:23 -0800

    recLinkedv3

--- recLinkedListFuncs.cpp
+++ recLinkedListFuncs.cpp
@@ -13,25 +13,20 @@ int recursiveSum(Node* head)
   Node* tempHolder = new Node;
   tempHolder->data = list->head->data; // holds previous sum
   head = list->head->next; // points head to next address
-  if (list->head == NULL)
+  if (head == NULL)
   {
     return 0;
   }
   else
   {
-  sum = tempHolder->data +list->head->data;
+    sum = tempHolder->data + list->head->data;
 
-  Node* tempNode = new Node;
-  tempNode->data = sum;
-  tempNode->next = list->head->next;
+    Node* tempNode = new Node;
+    tempNode->data = sum;
+    tempNode->next = list->head->next;
 
-  return (recursiveSum(tempNode));
+    return (recursiveSum(tempNode));
   }
- // int sum = 0;
-  //if (list->head == NULL) // end of the list
-  //{
-//	  return sum;
- // }//
 }
 
 
