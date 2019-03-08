cfadd9789654ef026c43aaa530049e50f07aaf93
Geovane Marquez <geovane@cs.ucsb.edu>
2019-03-07 19:49:54 -0800

    isAnagram.2

--- strFuncs.cpp
+++ strFuncs.cpp
@@ -13,21 +13,54 @@ using namespace std;
  */
 bool isAnagram(string s1, string s2)
 {
+  char arrAlphabet[26][2] = {{'A','a'}, {'B','b'}, {'C','c'}, {'D','d'},
+                    {'E','e'}, {'F','f'}, {'G','g'}, {'H','h'},
+                    {'I','i'}, {'J','j'}, {'K','k'}, {'L','l'},
+                    {'M','m'}, {'N','n'}, {'O','o'}, {'P','p'},
+                    {'Q','q'}, {'R','r'}, {'S','s'}, {'T','t'},
+                    {'U','u'}, {'V','v'}, {'W','w'}, {'X','x'},
+                    {'Y','y'}, {'Z','z'}};
+
   int strlength = s1.length();
-  string temp = s1;
-  for (int i = 0; i < strlength; i++)
+  string tempStr1 = s1;
+
+  for (int i = 0; i < strlength ; i++)
   {
     int countS1 = 0; //to count occurrances of s1
     int countS2 = 0; //to count occurrances of s2
     char c = s1[i]; //iterates through s1, char by char
 
-    for (int j = 0, k = 0; j < strlength; j++, k++)
+    /* The following for loop transforms an upper case
+       letter into a lower case letter
+    */
+    char tempChar = c;
+    char tempUpper;
+
+    for (int row = 0; row < 26; row++) //scan through the alphabet array
     {
-      if(c == temp[j])
+      if (tempChar == arrAlphabet[row][0] ||
+		      tempChar == arrAlphabet[row][1])
+      {
+        tempChar = arrAlphabet[row][1];
+        tempUpper = arrAlphabet[row][0];
+      }
+    } // tempChar is now lowercase
+    //char c can be upper or lowercase, not given yet
+    for (int k = 0; k < strlength; k++)
+    {
+      if (tempStr1[k] == tempUpper || tempStr1[k]== tempChar)
       {
         countS1++;
       }
-      if(c == s2[k])
+    }
+
+    char cLower = tempChar; // c is now lowercase no matter what
+    char cUpper = tempUpper; // uppercase char c or lowercase char c
+
+    for (int j = 0; j < strlength; j++) //
+    {
+      char d = s2[j];
+      if (d == cLower || d == cUpper)
       {
         countS2++;
       }
@@ -43,6 +76,8 @@ bool isAnagram(string s1, string s2)
   }
   return true;
 }
+
+
 
 /* Precondition: s1 is a valid string that may contain upper or lower case alphabets, no spaces or special characters
  * Postcondition: Returns true if s1 is a palindrome, false otherwise
